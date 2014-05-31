6cf0c954236857a7733587dde77ff1dd7c657e5b
silverice <slifeleaf@gmail.com>
2014-05-31 23:20:03 +0300

    regroup tests

--- JContainers/src/collections.tests.hpp
+++ JContainers/src/collections.tests.hpp
@@ -4,8 +4,6 @@
 
 namespace collections {
 
-#ifndef TEST_COMPILATION_DISABLED
-
     struct JCFixture : testing::Fixture {
         tes_context context;
 
@@ -18,26 +16,282 @@ namespace collections {
 #   define JC_TEST(name, name2) TEST_F(JCFixture, name, name2)
 #   define JC_TEST_DISABLED(name, name2) TEST_F(JCFixture, name, DISABLED_##name2)
 
+}
+
+#ifndef TEST_COMPILATION_DISABLED
+
+namespace collections {
+
+    const char * jsonTestString() {
+        const char *jsonString = STR(
+        {
+            "glossary": {
+                "title": "example glossary",
+                    "GlossDiv": {
+                        "title": "S",
+                            "GlossList": {
+                                "GlossEntry": {
+                                    "ID": "SGML",
+                                        "SortAs": "SGML",
+                                        "GlossTerm": "Standard Generalized Markup Language",
+                                        "Acronym": "SGML",
+                                        "Abbrev": "ISO 8879:1986",
+                                        "GlossDef": {
+                                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                                                "GlossSeeAlso": ["GML", "XML"]
+                                    },
+                                        "GlossSee": "markup"
+                                }
+                        }
+                }
+            },
+
+                "array": [["NPC Head [Head]", 0, -0.330000]]
+        }
+
+        );
+
+        return jsonString;
+    }
+
     JC_TEST(object_base, refCount)
     {
-        auto obj = array::create(context);
-        EXPECT_TRUE(obj->refCount() == 1);
-        obj->retain();
-        EXPECT_TRUE(obj->refCount() == 2);
+        {
+            auto obj = array::create(context);
+            EXPECT_TRUE(obj->refCount() == 1);
+            obj->retain();
+            EXPECT_TRUE(obj->refCount() == 2);
 
-        obj->release();
-        EXPECT_TRUE(obj->refCount() == 1);
-
-        obj->tes_retain();
-        obj->tes_retain();
-        EXPECT_TRUE(obj->refCount() == 1 + 2);
-        for (int i = 0; i < 20 ; i++) {
-        	obj->tes_release();
+            obj->release();
+            EXPECT_TRUE(obj->refCount() == 1);
         }
-        EXPECT_TRUE(obj->refCount() == 1);
+        {
+            auto obj = array::object(context);
+            EXPECT_TRUE(obj->refCount() == 0);
 
-        obj->release();
+            obj->retain();
+            EXPECT_TRUE(obj->refCount() == 1);
+
+            obj->tes_retain();
+            obj->tes_retain();
+            obj->tes_retain();
+            EXPECT_TRUE(obj->refCount() == 1 + 3);
+
+            // ensure that over-release does not affects internal ref count:
+            for (int i = 0; i < 20 ; i++) {
+                obj->tes_release();
+            }
+            EXPECT_TRUE(obj->refCount() == 1);
+
+            obj->release();
+            EXPECT_TRUE(obj->refCount() == 0);
+        }
     }
+
+    JC_TEST(Item, nulls)
+    {
+        Item i1;
+
+        EXPECT_TRUE(i1.isNull());
+
+        i1 = (const char*)nullptr;
+        EXPECT_TRUE(i1.isNull());
+
+        i1 = (TESForm*)nullptr;
+        EXPECT_TRUE(i1.isNull());
+
+        i1 = FormZero;
+        EXPECT_TRUE(i1.isNull());
+
+        i1 = (object_base *)nullptr;
+        EXPECT_TRUE(i1.isNull());
+    }
+
+    JC_TEST(Item, equality)
+    {
+        Item i1, i2;
+
+        EXPECT_TRUE(i1.isNull());
+        EXPECT_TRUE(i2.isNull());
+
+        EXPECT_TRUE(i1.isEqual(i1));
+        EXPECT_TRUE(i1.isEqual(i2));
+
+        i1 = "me";
+        i2 = "me";
+        EXPECT_TRUE(i1.isEqual(i2));
+
+        i2 = "not me";
+        EXPECT_FALSE(i1.isEqual(i2));
+
+        i1 = 1u;
+        i2 = 1u;
+        EXPECT_TRUE(i1.isEqual(i2));
+
+        i2 = 1.5f;
+        EXPECT_FALSE(i1.isEqual(i2));
+
+        auto obj = array::object(context);
+        i1 = obj;
+        i2 = obj;
+        EXPECT_TRUE(i1.isEqual(i2));
+    }
+
+    TEST(form_handling, test)
+    {
+        namespace fh = form_handling;
+
+        EXPECT_TRUE( fh::is_form_string("__formData|Skyrim.esm|0x1" ));
+        EXPECT_FALSE( fh::is_form_string("__formDatttt" ));
+        EXPECT_FALSE( fh::is_form_string(nullptr));
+
+        int pluginIdx = 0x9;
+        const char * pluginName = "Skyrim.esm";
+
+        // test static form ids
+        {
+            FormId form = (FormId)(pluginIdx << 24 | 0x14);
+            EXPECT_TRUE( fh::is_static(form) );
+
+            std::string formString = *fh::to_string(form, [=](int) { return pluginName; });
+            EXPECT_TRUE( formString == 
+                (std::string(fh::kFormData) + fh::kFormDataSeparator + pluginName + fh::kFormDataSeparator + "0x14"));
+
+            EXPECT_TRUE( form == 
+                *fh::from_string(formString.c_str(), [=](const char*) { return pluginIdx; }) );
+
+        }
+
+        // test global (0xFF*) form ids
+        {
+            FormId form = (FormId)(FormGlobalPrefix << 24 | 0x14);
+            EXPECT_TRUE( !fh::is_static(form) );
+
+            std::string formString = *fh::to_string(form, [=](int) { return pluginName; });
+
+            EXPECT_TRUE( formString == 
+                (std::string(fh::kFormData) + fh::kFormDataSeparator + fh::kFormDataSeparator + "0xff000014"));
+
+            EXPECT_TRUE( form == 
+                *fh::from_string(formString.c_str(), [=](const char*) { return pluginIdx; }) );
+        }
+    }
+
+    JC_TEST(json_deserializer, test)
+    {
+        EXPECT_FALSE( json_deserializer::object_from_file(context, "") );
+        EXPECT_FALSE( json_deserializer::object_from_file(context, nullptr) );
+
+        EXPECT_FALSE( json_deserializer::object_from_json_data(context, "") );
+        EXPECT_FALSE( json_deserializer::object_from_json_data(context, nullptr) );
+    }
+
+    JC_TEST(json_serializer, test)
+    {
+        auto obj = json_deserializer::object_from_json_data(context, jsonTestString());
+        EXPECT_TRUE(obj != nullptr);
+
+        auto data = json_serializer::create_json_data(*obj);
+        //printf("%s", data.get());
+    }
+
+    JC_TEST(json_serializer, no_infinite_recursion)
+    {
+        map *cnt = map::object(context);
+        cnt->u_setValueForKey("cycle", Item(cnt));
+
+        json_serializer::create_json_data(*cnt);
+    }
+}
+
+namespace collections {
+
+    JC_TEST(array,  test)
+    {
+        auto arr = array::create(context);
+
+        EXPECT_TRUE(tes_array::count(arr) == 0);
+
+        auto str = 10;
+        tes_array::addItemAt<SInt32>(arr, str);
+        EXPECT_TRUE(tes_array::count(arr) == 1);
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(arr, 0) == 10);
+
+        str = 30;
+        tes_array::addItemAt<SInt32>( arr, str);
+        EXPECT_TRUE(tes_array::count(arr) == 2);
+        EXPECT_TRUE(tes_array::itemAtIndex<SInt32>(arr, 1) == 30);
+
+        auto arr2 = array::create(context);
+        tes_array::addItemAt<SInt32>(arr2, 4);
+
+        tes_array::addItemAt(arr, arr2);
+        EXPECT_TRUE(tes_array::itemAtIndex<Handle>(arr, 2) == arr2->uid());
+
+        tes_object::release(arr);
+
+        EXPECT_TRUE(tes_array::itemAtIndex<SInt32>( arr2, 0) == 4);
+
+        tes_object::release( arr2);
+    }
+
+    JC_TEST(array,  negative_indices)
+    {
+        auto obj = array::object(context);
+
+        SInt32 values[] = {1,2,3,4,5,6,7};
+
+        for (auto num : values) {
+            tes_array::addItemAt(obj, num, -1);
+        }
+
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -1) == 7);
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 6);
+
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 0) == 1);
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 1) == 2);
+
+        tes_array::addItemAt(obj, 8, -2);
+        //{1,2,3,4,5,6,8,7}
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 8);
+
+        tes_array::addItemAt(obj, 0, 0);
+        //{0,1,2,3,4,5,6,8,7}
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 0) == 0);
+
+        tes_array::eraseIndex(obj, -1);
+        //{0,1,2,3,4,5,6,8}
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -1) == 8);
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 7) == 8);
+
+        tes_array::eraseIndex(obj, -2);
+        //{0,1,2,3,4,5,8}
+        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 5);
+
+    }
+
+    TEST(tes_jcontainers, tes_jcontainers)
+    {
+        EXPECT_TRUE(tes_jcontainers::isInstalled());
+
+        EXPECT_FALSE(tes_jcontainers::fileExistsAtPath(nullptr));
+        EXPECT_TRUE(tes_jcontainers::fileExistsAtPath("JContainers.dll"));
+        EXPECT_TRUE(!tes_jcontainers::fileExistsAtPath("abracadabra"));
+    }
+
+    JC_TEST(map, key_case_insensitivity)
+    {
+        map *cnt = map::object(context);
+
+        std::string name = "back in black";
+        cnt->u_setValueForKey("ACDC", Item(name));
+
+        EXPECT_TRUE(*cnt->u_find("acdc")->stringValue() == name);
+    }
+
+}
+
+namespace collections {
 
     JC_TEST(tes_context, database)
     {
@@ -97,10 +351,16 @@ namespace collections {
 
         for (int i = 0; i < 10; ++i) {
             auto obj = map::create(context);//+1, rc is 1
+            EXPECT_TRUE(obj->refCount() == 1);
 
             obj->release();//-1, rc is 0, add to delete queue
+            EXPECT_TRUE(obj->refCount() == 0);
+
+            obj->release();//rc is 0
+            EXPECT_TRUE(obj->refCount() == 0);
 
             obj->retain();// +1, rc is 1
+            EXPECT_TRUE(obj->refCount() == 1);
 
             identifiers.push_back(obj->uid());
         }
@@ -111,8 +371,9 @@ namespace collections {
             });
         };
 
-        std::this_thread::sleep_for( std::chrono::seconds(13) );
+        std::this_thread::sleep_for( std::chrono::seconds(14) );
 
+        //
         EXPECT_TRUE(allExist());
     }
 
@@ -144,42 +405,7 @@ namespace collections {
         EXPECT_TRUE(allExist() == false);
     }
 
-
-#define STR(...)    #__VA_ARGS__
-
-    const char * jsonTestString() {
-        const char *jsonString = STR(
-        {
-            "glossary": {
-                "title": "example glossary",
-                    "GlossDiv": {
-                        "title": "S",
-                            "GlossList": {
-                                "GlossEntry": {
-                                    "ID": "SGML",
-                                        "SortAs": "SGML",
-                                        "GlossTerm": "Standard Generalized Markup Language",
-                                        "Acronym": "SGML",
-                                        "Abbrev": "ISO 8879:1986",
-                                        "GlossDef": {
-                                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                                                "GlossSeeAlso": ["GML", "XML"]
-                                    },
-                                        "GlossSee": "markup"
-                                }
-                        }
-                }
-            },
-
-                "array": [["NPC Head [Head]", 0, -0.330000]]
-        }
-
-        );
-
-        return jsonString;
-    }
-
-    TEST(json_handling, collection_operators)
+    TEST(path_resolving, collection_operators)
     {
         auto shouldReturnNumber = [&](object_base *obj, const char *path, float value) {
             path_resolving::resolvePath(obj, path, [&](Item * item) {
@@ -295,162 +521,6 @@ namespace collections {
         });
     }
 
-    JC_TEST(json_deserializer, test)
-    {
-        EXPECT_FALSE( json_deserializer::object_from_file(context, "") );
-        EXPECT_FALSE( json_deserializer::object_from_file(context, nullptr) );
-
-        EXPECT_FALSE( json_deserializer::object_from_json_data(context, "") );
-        EXPECT_FALSE( json_deserializer::object_from_json_data(context, nullptr) );
-    }
-
-    JC_TEST(json_serializer, test)
-    {
-        auto obj = json_deserializer::object_from_json_data(context, jsonTestString());
-        EXPECT_TRUE(obj != nullptr);
-
-        auto data = json_serializer::create_json_data(*obj);
-        printf("%s", data.get());
-    }
-
-    JC_TEST(Item, equality)
-    {
-        Item i1, i2;
-
-        EXPECT_TRUE(i1.isNull());
-        EXPECT_TRUE(i2.isNull());
-
-        EXPECT_TRUE(i1.isEqual(i1));
-        EXPECT_TRUE(i1.isEqual(i2));
-
-        i1 = "me";
-        i2 = "me";
-        EXPECT_TRUE(i1.isEqual(i2));
-
-        i2 = "not me";
-        EXPECT_FALSE(i1.isEqual(i2));
-
-        i1 = 1u;
-        i2 = 1u;
-        EXPECT_TRUE(i1.isEqual(i2));
-
-        i2 = 1.5f;
-        EXPECT_FALSE(i1.isEqual(i2));
-
-        i1 = (const char*)nullptr;
-        EXPECT_TRUE(i1.isNull());
-
-        i1 = (TESForm*)nullptr;
-        EXPECT_TRUE(i1.isNull());
-
-        i1 = FormZero;
-        EXPECT_TRUE(i1.isNull());
-
-
-        auto obj = array::object(context);
-        i1 = obj;
-        i2 = obj;
-        EXPECT_TRUE(i1.isEqual(i2));
-    }
-   
-/*
-    TEST(saveAll, test)
-    {
-        saveAll();
-    }*/
-
-    JC_TEST(array,  test)
-    {
-        auto arr = array::create(context);
-
-        EXPECT_TRUE(tes_array::count(arr) == 0);
-
-        auto str = 10;
-        tes_array::addItemAt<SInt32>(arr, str);
-        EXPECT_TRUE(tes_array::count(arr) == 1);
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(arr, 0) == 10);
-
-        str = 30;
-        tes_array::addItemAt<SInt32>( arr, str);
-        EXPECT_TRUE(tes_array::count(arr) == 2);
-        EXPECT_TRUE(tes_array::itemAtIndex<SInt32>(arr, 1) == 30);
-
-        auto arr2 = array::create(context);
-        tes_array::addItemAt<SInt32>(arr2, 4);
-
-        tes_array::addItemAt(arr, arr2);
-        EXPECT_TRUE(tes_array::itemAtIndex<Handle>(arr, 2) == arr2->uid());
-
-        tes_object::release(arr);
-
-        EXPECT_TRUE(tes_array::itemAtIndex<SInt32>( arr2, 0) == 4);
-
-        tes_object::release( arr2);
-    }
-
-    JC_TEST(array,  negative_indices)
-    {
-        auto obj = array::object(context);
-
-        SInt32 values[] = {1,2,3,4,5,6,7};
-
-        for (auto num : values) {
-            tes_array::addItemAt(obj, num, -1);
-        }
-
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -1) == 7);
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 6);
-
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 0) == 1);
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 1) == 2);
-        
-        tes_array::addItemAt(obj, 8, -2);
-        //{1,2,3,4,5,6,8,7}
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 8);
-
-        tes_array::addItemAt(obj, 0, 0);
-        //{0,1,2,3,4,5,6,8,7}
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 0) == 0);
-
-        tes_array::eraseIndex(obj, -1);
-        //{0,1,2,3,4,5,6,8}
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -1) == 8);
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, 7) == 8);
-
-        tes_array::eraseIndex(obj, -2);
-        //{0,1,2,3,4,5,8}
-        EXPECT_TRUE( tes_array::itemAtIndex<SInt32>(obj, -2) == 5);
-
-    }
-
-    TEST(tes_jcontainers, tes_jcontainers)
-    {
-        EXPECT_TRUE(tes_jcontainers::isInstalled());
-
-        EXPECT_FALSE(tes_jcontainers::fileExistsAtPath(nullptr));
-        EXPECT_TRUE(tes_jcontainers::fileExistsAtPath("JContainers.dll"));
-        EXPECT_TRUE(!tes_jcontainers::fileExistsAtPath("abracadabra"));
-    }
-
-    JC_TEST(map, key_case_insensitivity)
-    {
-        map *cnt = map::object(context);
-
-        std::string name = "back in black";
-        cnt->u_setValueForKey("ACDC", Item(name));
-
-        EXPECT_TRUE(*cnt->u_find("acdc")->stringValue() == name);
-    }
-
-
-    JC_TEST(json_handling, no_infinite_recursion)
-    {
-        map *cnt = map::object(context);
-        cnt->u_setValueForKey("cycle", Item(cnt));
-
-        json_serializer::create_json_data(*cnt);
-    }
-
     JC_TEST_DISABLED(dl, dl)
     {
         auto obj = map::object(context);
@@ -494,47 +564,6 @@ namespace collections {
         fut1.wait();
         fut2.wait();
     }
-
-    TEST(json_handling, form_serialization)
-    {
-        namespace fh = form_handling;
-
-        EXPECT_TRUE( fh::is_form_string("__formData|Skyrim.esm|0x1" ));
-        EXPECT_FALSE( fh::is_form_string("__formDatttt" ));
-        EXPECT_FALSE( fh::is_form_string(nullptr));
-
-        int pluginIdx = 0x9;
-        const char * pluginName = "Skyrim.esm";
-
-        FormId form = (FormId)(pluginIdx << 24 | 0x14);
-
-        std::string formString = *fh::to_string(form, [=](int) { return pluginName; });
-        EXPECT_TRUE( formString == 
-            (std::string(fh::kFormData) + fh::kFormDataSeparator + pluginName + fh::kFormDataSeparator + "0x14"));
-
-        EXPECT_TRUE( form == 
-            *fh::from_string(formString.c_str(), [=](const char*) { return pluginIdx; }) );
-    }
-
-    TEST(json_handling, form_serialization_global)
-    {
-        namespace fh = form_handling;
-
-        int pluginIdx = 0x9;
-        const char * pluginName = "Skyrim.esm";
-        FormId form = (FormId)(FormGlobalPrefix << 24 | 0x14);
-
-        std::string formString = *fh::to_string(form, [=](int) { return pluginName; });
-
-        EXPECT_TRUE( formString == 
-            (std::string(fh::kFormData) + fh::kFormDataSeparator + fh::kFormDataSeparator + "0xff000014"));
-
-        EXPECT_TRUE( form == 
-            *fh::from_string(formString.c_str(), [=](const char*) { return pluginIdx; }) );
-    }
-
-
- 
-    #endif
 }
 
+#endif
