ee5e18b07989a5b8b4427c8fbe159fb1dc5503ac
silverice <slifeleaf@gmail.com>
2014-07-19 02:11:41 +0300

    add current_range property which moves through all empty ranges, filling them with identifiers

--- id_generator/id_generator.cpp
+++ id_generator/id_generator.cpp
@@ -65,23 +65,36 @@ struct range {
 struct id_generator {
     //
     std::deque<range> _empty_ranges;
+    std::deque<range>::iterator _current_range;
 
     id_generator() {
         _empty_ranges.push_back(
-            range::with_first_last(1, std::numeric_limits<id>::max() - 1)
+            range::with_first_last(1, /*std::numeric_limits<id>::max() - 1*/12000)
             );
+
+        _current_range = _empty_ranges.begin();
     }
 
     id newId() {
         assert(_empty_ranges.empty() == false);
-        range& rn = _empty_ranges.back();
+        range& rn = *_current_range;
         auto num = rn.shrink_front();
+
         if (rn.empty()) {
-            _empty_ranges.pop_back();
+            _current_range = _empty_ranges.erase(_current_range);
+
+            if (_current_range == _empty_ranges.end()) {
+                _current_range = _empty_ranges.begin();
+            }
         }
 
         return num;
     }
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
 
     void reuse_id(id val) {
         range fake = range::with_first_last(val, val);
@@ -94,22 +107,45 @@ struct id_generator {
         range *rightRange = (right < _empty_ranges.end() ? &*(right) : nullptr);
         rightRange = (rightRange && rightRange->begin() - 1 == val ? rightRange : nullptr);
 
-        if (leftRange) {
-            leftRange->last = val;
-        }
+        if (!leftRange && !rightRange) {
+            ++d;
+            size_t idx = _current_range - _empty_ranges.begin() + (_current_range < right ? 0 : 1);
+            _empty_ranges.insert(right, fake);
+            _current_range = _empty_ranges.begin() + idx;
 
-        if (rightRange) {
-            rightRange->first = val;
-        }
+/*
+            if (_current_range == _empty_ranges.end()) {
+                _current_range = _empty_ranges.begin();
+            }
+*/
 
-        if (rightRange && leftRange) {
+            assert(_current_range != _empty_ranges.end());
+        }
+        else if (rightRange && leftRange) {
+            ++a;
             // merge ranges
             leftRange->last = rightRange->last;
+            // _current_range becomes ivnalid each time we modify _empty_ranges
+            size_t idx = _current_range - _empty_ranges.begin() - (_current_range < right ? 0 : 1);
             _empty_ranges.erase(right);
-        }
+            _current_range = _empty_ranges.begin() + idx;
 
-        if (!leftRange && !rightRange) {
-            _empty_ranges.insert(right, fake);
+/*
+            if (_current_range == _empty_ranges.end()) {
+                _current_range = _empty_ranges.begin();
+            }
+*/
+
+
+            assert(_current_range != _empty_ranges.end());
+        }
+        else if (leftRange) {
+            ++b;
+            leftRange->last = val;
+        }
+        else if (rightRange) {
+            ++c;
+            rightRange->first = val;
         }
     }
 };
@@ -122,13 +158,13 @@ int main(int argc, char* argv[])
 
     std::deque<id> ids;
 
-    for (int i  = 0; i < 100; ++i) {
+    for (int i  = 0; i < 1000; ++i) {
 
-        for (int d = 0; d < 253; ++d) {
+        for (int d = 0; d < 50; ++d) {
             ids.push_back( gen.newId() );
         }
 
-        for (int j = 0; j < 252; ++j) {
+        for (int j = 0; j < 40; ++j) {
             auto itr = ids.begin() + rand() % ids.size();
             gen.reuse_id(*itr);
             ids.erase(itr);
