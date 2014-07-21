6d9c6f7480ac666e42a1d39413cf071e5628670c
silverice <slifeleaf@gmail.com>
2014-07-21 03:09:15 +0300

    use, test new id generator

--- JContainers/src/id_generator.h
+++ JContainers/src/id_generator.h
@@ -2,16 +2,14 @@
 
 namespace collections {
 
-/*
+
     template<class ID>
-    class id_generator {
+    struct id_generator_0 {
         std::vector<ID> m_freeNums;
-        ID m_highest;
+        ID m_highest; // i.e. [m_highest + 1, max(ID)] is free range
 
-    public:
-
-        id_generator() : m_highest(0) {}
-
+        id_generator_0() : m_highest(0) {}
+        /*
         ID newId() {
             if (!m_freeNums.empty()) {
                 int num = m_freeNums.back();
@@ -27,24 +25,26 @@ namespace collections {
             m_freeNums.push_back(num);
         }
 
-        template<class Archive>
-        void serialize(Archive & ar, const unsigned int version) {
-            ar & m_freeNums;
-            ar & m_highest;
-        }
-
         void u_clear() {
             m_highest = 0;
             m_freeNums.clear();
         }
+        */
+        template<class Archive>
+        void load(Archive & ar) {
+            ar & m_freeNums;
+            ar & m_highest;
+        }
     };
-*/
 
 
-    template<class id>
-    struct id_generator {
+    template<
+        class id,
+        id min_identifier,
+        id max_identifier
+    >
+    struct id_generator : boost::noncopyable {
 
-        //
         struct range {
             id first;
             id last;
@@ -57,28 +57,7 @@ namespace collections {
                 return first > last;
             }
 
-            bool is_near(id value) {
-
-            }
-
-            bool contains_or_near(id value) const {
-                return
-                    (value >= first - 1 && value <= end());
-            }
-
-            bool merge_if_can(id value) {
-                if (contains_or_near(value)) {
-
-                    if (value <= end()) {
-                    }
-
-                    return true;
-                }
-
-                return false;
-            }
-
-            id shrink_front() {
+            id new_id() {
                 assert(empty() == false);
                 return first++;
             }
@@ -95,6 +74,10 @@ namespace collections {
                 return first < other.first;
             }
 
+            bool operator > (const range& other) const {
+                return first > other.first;
+            }
+
             template<class Archive>
             void serialize(Archive & ar, const unsigned int version) {
                 ar & first;
@@ -109,10 +92,10 @@ namespace collections {
             u_clear();
         }
 
-        id newId() {
+        id new_id() {
             assert(_empty_ranges.empty() == false);
             range& rn = *_current_range;
-            auto num = rn.shrink_front();
+            auto num = rn.new_id();
 
             if (rn.empty()) {
                 _current_range = _empty_ranges.erase(_current_range);
@@ -125,69 +108,58 @@ namespace collections {
             return num;
         }
 
-
-        friend class boost::serialization::access;
-        BOOST_SERIALIZATION_SPLIT_MEMBER();
-
-
-        template<class Archive>
-        void save(Archive & ar, const unsigned int version) const {
-            ar << _empty_ranges;
-            size_t currIdx = _current_range - _empty_ranges.begin();
-            ar << currIdx;
-        }
-
-        template<class Archive>
-        void load(Archive & ar, const unsigned int version) {
-            ar >> _empty_ranges;
-            size_t currIdx = 0;
-            ar >> currIdx;
-
-            _current_range = _empty_ranges.begin() + currIdx;
-        }
-
-
         void u_clear() {
             _empty_ranges = {
-                range::with_first_last(1, /*std::numeric_limits<id>::max() - 1*/12000)
+                range::with_first_last(min_identifier, max_identifier)
             };
-
             _current_range = _empty_ranges.begin();
         }
 
-
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-
-        void reuseId(id val) {
+        bool is_free_id(id val) const {
             range fake = range::with_first_last(val, val);
-            // itr >= val
+            // right > val
+            auto right = std::upper_bound(_empty_ranges.begin(), _empty_ranges.end(), fake);
+            const range *leftRange = (right > _empty_ranges.begin() ? &*(right - 1) : nullptr);
+
+            return (leftRange && leftRange->begin() <= val && val < leftRange->end());
+        }
+
+        bool is_valid() const {
+            auto noOverlap = [&]() {
+                for (size_t i = 1; i < _empty_ranges.size(); ++i) {
+                    const auto& prev = _empty_ranges[i - 1];
+                    const auto& curr = _empty_ranges[i];
+
+                    if (!(prev.begin() < curr.begin() && prev.end() < curr.end())) {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+
+            return !_empty_ranges.empty() && _current_range != _empty_ranges.end() && noOverlap();
+        }
+
+        void reuse_id(id val) {
+            range fake = range::with_first_last(val, val);
+            // right >= val
             auto right = std::lower_bound(_empty_ranges.begin(), _empty_ranges.end(), fake);
 
             range *leftRange = (right > _empty_ranges.begin() ? &*(right - 1) : nullptr);
             leftRange = (leftRange && leftRange->end() == val ? leftRange : nullptr);
 
-            range *rightRange = (right < _empty_ranges.end() ? &*(right) : nullptr);
+            range *rightRange = (right != _empty_ranges.end() ? &*(right) : nullptr);
             rightRange = (rightRange && rightRange->begin() - 1 == val ? rightRange : nullptr);
 
             if (!leftRange && !rightRange) {
-                ++d;
                 size_t idx = _current_range - _empty_ranges.begin() + (_current_range < right ? 0 : 1);
                 _empty_ranges.insert(right, fake);
                 _current_range = _empty_ranges.begin() + idx;
 
-                /*
-                if (_current_range == _empty_ranges.end()) {
-                _current_range = _empty_ranges.begin();
-                }
-                */
-
                 assert(_current_range != _empty_ranges.end());
             }
             else if (rightRange && leftRange) {
-                ++a;
                 // merge ranges
                 leftRange->last = rightRange->last;
                 // _current_range becomes ivnalid each time we modify _empty_ranges
@@ -195,25 +167,112 @@ namespace collections {
                 _empty_ranges.erase(right);
                 _current_range = _empty_ranges.begin() + idx;
 
-                /*
-                if (_current_range == _empty_ranges.end()) {
-                _current_range = _empty_ranges.begin();
-                }
-                */
-
-
                 assert(_current_range != _empty_ranges.end());
             }
             else if (leftRange) {
-                ++b;
                 leftRange->last = val;
             }
             else if (rightRange) {
-                ++c;
                 rightRange->first = val;
             }
         }
+
+        //////////////////////////////////////////////////////////////////////////
+        
+        friend class boost::serialization::access;
+        BOOST_SERIALIZATION_SPLIT_MEMBER();
+
+        template<class Archive>
+        void save(Archive & ar, const unsigned int version) const {
+            size_t currIdx = _current_range - _empty_ranges.begin();
+            ar << _empty_ranges << currIdx;
+        }
+
+        template<class Archive>
+        void load(Archive & ar, const unsigned int version) {
+
+            switch (version)
+            {
+            default:
+                jc_assert(false);
+                break;
+            case 1: {
+                size_t currIdx = 0;
+
+                ar >> _empty_ranges >> currIdx;
+
+                _current_range = _empty_ranges.begin() + currIdx;
+            }
+                break;
+            case 0:
+                load_0(ar);
+                break;
+            }
+        }
+
+        template <class Archive> void load_0(Archive& ar) {
+            _empty_ranges.clear();
+            _current_range = _empty_ranges.end();
+
+            id_generator_0<id> old;
+            old.load(ar);
+
+            std::sort(old.m_freeNums.begin(), old.m_freeNums.end());
+            std::unique(old.m_freeNums.begin(), old.m_freeNums.end());
+
+            for (const auto& num : old.m_freeNums) {
+
+                if (!_empty_ranges.empty() && num - _empty_ranges.back().last == 1) {
+                    _empty_ranges.back().last = num;
+                }
+                else {
+                    _empty_ranges.push_back(range::with_first_last(num, num));
+                }
+            }
+
+            _empty_ranges.push_back(range::with_first_last(old.m_highest + 1, max_identifier));
+            _current_range = _empty_ranges.end() - 1;
+        }
     };
 
+    // 2147483647 - 2 identifiers is not enough? anyone?
+    typedef id_generator<HandleT, 1, 0x7FFFFFFF - 1> id_generator_type;
+
+
+#   ifndef TEST_COMPILATION_DISABLED
+
+    TEST(id_generator, t)
+    {
+        srand((unsigned int)time(nullptr));
+
+        typedef uint8_t id_t;
+
+        id_generator<id_t, 1, 200> gen;
+
+        std::deque<id_t> ids;
+
+        for (int i = 0; i < 100; ++i) {
+
+            for (int d = 0; d < 50; ++d) {
+                auto id = gen.new_id();
+                EXPECT_TRUE(std::find(ids.begin(), ids.end(), id) == ids.end()); // no duplicates
+                ids.push_back(id);
+
+                EXPECT_TRUE(gen.is_valid());
+            }
+
+            for (int j = 0; j < 49; ++j) {
+                auto itr = ids.begin() + rand() % ids.size();
+                gen.reuse_id(*itr);
+                ids.erase(itr);
+
+                EXPECT_TRUE(gen.is_valid());
+            }
+        }
+    }
+
+#   endif
 
 }
+
+BOOST_CLASS_VERSION(collections::id_generator_type, 1);
