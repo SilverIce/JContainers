34b9e286234507ecfc4b9e2e73b784b2e20f388a
ryobg <ryobg@users.noreply.github.com>
2018-02-13 00:21:47 +0200

    Use proper output folders when creating the distro. Allow Boost to be build incrementally (or just remove the temp obj generated).

--- build_boost.bat
+++ build_boost.bat
@@ -3,10 +3,12 @@ REM https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z
 
 cd dep\boost\
 
-if not exist b2.exe (
+if not exist b2.exe ( 
     call bootstrap.bat
 )
 
 b2 variant=release,debug link=static threading=multi runtime-link=static address-model=64 --with-serialization --with-date_time --with-filesystem --with-thread --build-dir=garbage --stagedir=boost_lib stage
+
+REM RMDIR /Q /S garbage
 
 cd ..\..\
--- post-build/jc_install.py
+++ post-build/jc_install.py
@@ -87,9 +87,13 @@ class Config(object):
         return os.path.join(self.origin, 'Data\SKSE\Plugins')
 
     @property
+    def jcLibPath(self):
+        return os.path.join(self.pluginDir, JCLib.dllName())
+
+    @property
     def jcLib(self):
         if not self._jcLib:
-            self._jcLib = JCLib(os.path.join(self.pluginDir, JCLib.dllName()))
+            self._jcLib = JCLib(self.jcLibPath)
 
         return self._jcLib
 
@@ -140,6 +144,7 @@ def copyTree(root_src_dir, root_dst_dir):
 
 def setupSkyrimTree(config):
     copyTree('../JContainers/Data', config.dataDir)
+    shutil.copy2 (os.path.join (config.origin, JCLib.dllName ()), config.jcLibPath)
 
 
 def recreatePath(*args):
@@ -192,7 +197,7 @@ def bundleDevResources(mode):
     cleanCopyTree(os.path.join(ROOT, 'api_usage_example'), devr, ignore = lambda x,y: ['Release', 'Debug'])
     shutil.copy2(os.path.join(ROOT, 'JContainers/src/jc_interface.h'), os.path.join(devr, 'api_usage_example'))
 
-    shutil.copy2(os.path.join(ROOT, 'x64', 'Release', 'json_validator.exe'), devr)
+    shutil.copy2(os.path.join(ROOT, mode, 'json_validator.exe'), devr)
 
     make_archive(devr, devr + "\*")
     shutil.rmtree(devr, ignore_errors = True)
