b99eb135fb50d5aa6b487a370e18535f25de89e6
ryobg <ryobg@users.noreply.github.com>
2018-02-12 22:52:21 +0200

    Made more explicit that we build Boost by invoking install.bat (now build_boost.bat). Also do only x64 builds for sanity.

--- dep/boost/build_libraries.bat
+++
@@ -1,7 +0,0 @@
-REM https://sourceforge.net/projects/boost/files/boost/1.66.0/
-
-b2 variant=release,debug link=static threading=multi runtime-link=static --with-serialization --with-date_time --with-filesystem --with-thread --build-dir=garbage --stagedir=boost_lib stage
-
-
-DEL /q /s garbage
-
