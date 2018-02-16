6955c42ee30c27bd872a79b6e0916078fe1e7d23
ryobg <ryobg@users.noreply.github.com>
2018-02-16 11:17:10 +0200

    Chasing for several hours inconsistencies between Bjam (not recreating its temp cmd files), Windows (not deleting them even after restart), Boost (not adding x64 on lib when its apparent) and MSVC (for breaking compatibilities between patch versions)... Darn!

--- tools/build_boost.bat
+++ tools/build_boost.bat
@@ -1,38 +1,30 @@
 @echo off
 setlocal
 
-set BOOST_ROOT="%~dp0\..\dep\boost"
+set BOOST_ROOT=%~dp0\..\dep\boost
 
 REM Zero the error level
 ver > nul
 
 if not exist "%BOOST_ROOT%\bootstrap.bat" (
 
-    for /f %%p in ('where python3') do set PYTHON_EXE=%%p
-    if not defined PYTHON_EXE (
-        for /f %%p in ('where python') do set PYTHON_EXE=%%p
-        if not defined PYTHON_EXE (
-            echo No Python 3 accessible?
+    if not exist "%~dp0\boost.7z" (
+
+        echo Download Boost...
+        python "%~dp0\download.py" ^
+            https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z ^
+            "%~dp0\boost.7z"
+        if errorlevel 1 (
+            echo Unable to download Boost, exiting...
             exit /b 1
         )
-    )
 
-    echo Download Boost...
-    REM https://sourceforge.net/projects/boost/files/boost/1.66.0/
-    REM https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z
-    %PYTHON_EXE% "%~dp0\download.py" ^
-        https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z ^
-        "%~dp0\boost.7z"
-    if errorlevel 1 (
-        echo Unable to download Boost, exiting...
-        exit /b 1
-    )
-
-    echo Rename Boost...
-    "%~dp0\7za.exe" rn "%~dp0\boost.7z" boost_1_66_0 boost
-    if errorlevel 1 (
-        echo Unable to rename Boost, exiting...
-        exit /b 1
+        echo Rename Boost folder...
+        "%~dp0\7za.exe" rn "%~dp0\boost.7z"  boost_1_66_0 boost
+        if errorlevel 1 (
+            echo Unable to rename Boost, exiting...
+            exit /b 1
+        )
     )
 
     echo Unpack Boost...
@@ -55,7 +47,8 @@ if not exist b2.exe (
     )
 )
 
-b2 variant=release,debug link=static threading=multi runtime-link=static address-model=64 ^
+b2 architecture=x86 address-model=64 variant=release,debug ^
+  link=static runtime-link=static threading=multi ^
  --with-serialization --with-date_time --with-filesystem --with-thread ^
  --build-dir=garbage --stagedir=boost_lib stage
 
