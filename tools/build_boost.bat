bfad8e536206091180a7824d4fe2a7ba8469bd36
Denys <SilverIce@users.noreply.github.com>
2021-09-27 15:47:24 -0700

    Fix boost archive url

--- tools/build_boost.bat
+++ tools/build_boost.bat
@@ -12,7 +12,7 @@ if not exist "%BOOST_ROOT%\bootstrap.bat" (
 
         echo Download Boost...
         python "%~dp0\download.py" ^
-            https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.7z ^
+            https://boostorg.jfrog.io/artifactory/main/release/1.67.0/source/boost_1_67_0.7z ^
             "%~dp0\boost.7z"
         if errorlevel 1 (
             echo Unable to download Boost, exiting...
